60f6060634c38dd5a60ffc9843be2ae788b9386c
Akshyat Pathak <114989068+mysojinyeast@users.noreply.github.com>
2024-02-14 19:29:12 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Todo App', () => {
+  it('renders the todo application', () => {
+    render(<App />);
+    const headerElement = screen.getByText(/Manage Your Todos/i);
+    expect(headerElement).toBeInTheDocument();
+  });
+
+  it('adds a new todo', () => {
+    render(<App />);
+    
+    
+    const inputField = screen.getByPlaceholderText('Write Todo...');
+    const addButton = screen.getByText(/Add/i);
+
+    
+    
+    
+  });
+
+  
+  //it('toggles the completion status of a todo', () => {
+    //render(<App />);
+
+    
+    //const todoElement = screen.getByText(/Some Todo/i);
+    //const checkbox = todoElement.parentElement.querySelector('input[type="checkbox"]');
+
+    
+    //fireEvent.click(checkbox);
+
+    
+    //expect(checkbox.checked).toBe(true); 
+  //});
+
+  
 });
